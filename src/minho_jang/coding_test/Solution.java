199f10646be36f150762d071949fb4069aa2b8d1
장민호 <minho9301@naver.com>
2021-01-08 22:53:31 +0900

    New: Coding test at 2021.01.08 (Not Solved)

--- src/minho_jang/coding_test/Solution.java
+++ src/minho_jang/coding_test/Solution.java
@@ -3,5 +3,9 @@ package minho_jang.coding_test;
 import java.util.*;
 
 class Solution {
-    
+    public static void main(String[] args) {
+    	ArrayList<Integer> tmp = new ArrayList<>();
+    	int[] arr = {1,2,3,4};
+    	
+    }
 }
