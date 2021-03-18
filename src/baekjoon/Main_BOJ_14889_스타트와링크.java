2452696d62348d49e6813c18c34d8ea9090cf720
minho-jang <minho9301@naver.com>
2021-03-18 17:16:58 +0900

    Solve: BOJ 14889 스타트와링크, SWEA 3289 서로소집합, JW 1863 종교

---
+++ src/baekjoon/Main_BOJ_14889_스타트와링크.java
@@ -0,0 +1,76 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_BOJ_14889_스타트와링크 {
+	static int N, answer;
+	static int[][] stat;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk;
+		
+		answer = Integer.MAX_VALUE;
+		N = Integer.parseInt(br.readLine());
+		stat = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			stk = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				stat[i][j] = Integer.parseInt(stk.nextToken());
+			}
+		}
+		
+		comb(0, 0, new int[N/2]);
+		System.out.println(answer);
+	}
+	
+	private static void comb(int cnt, int start, int[] res) {
+		if (cnt == N/2) {
+			// 조합완성
+			
+			int[] res2 = new int[N/2];
+			int idx = 0;
+			for (int i = 0; i < N; i++) {
+				boolean flag = true;
+				
+				for (int j = 0; j < N/2; j++) {
+					if (i == res[j])
+						flag = false;
+				}
+				if (flag)
+					res2[idx++] = i;
+			}
+			
+//			System.out.println(Arrays.toString(res));
+//			System.out.println(Arrays.toString(res2));
+			
+			int sum1 = getStatSum(res);
+			int sum2 = getStatSum(res2);
+//			System.out.println(sum1 + " " + sum2);
+			
+			answer = Math.min(answer, Math.abs(sum1-sum2));
+			
+			return;
+		}
+		
+		for (int i = start; i < N; i++) {
+			res[cnt] = i;
+			comb(cnt+1, i+1, res);
+		}
+	}
+
+	private static int getStatSum(int[] team) {
+		int sum = 0;
+		for (int i = 0; i < team.length; i++) {
+			for (int j = 0; j < team.length; j++) {
+				sum += stat[team[i]][team[j]];
+			}
+		}
+		
+		return sum;
+	}
+}
---
+++ src/swea/Solution_3289_서로소집합.java
@@ -0,0 +1,64 @@
+package swea;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution_3289_서로소집합 {
+	static int N;
+	static int[] parents;
+	
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk;
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			StringBuilder answer = new StringBuilder();
+			answer.append("#").append(tc).append(" ");
+			
+			stk = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(stk.nextToken());
+			parents = new int[N+1];
+			Arrays.fill(parents, -1);
+			
+			int M = Integer.parseInt(stk.nextToken());
+			for (int i = 0; i < M; i++) {
+				stk = new StringTokenizer(br.readLine());
+				int isUnion = Integer.parseInt(stk.nextToken());
+				int a = Integer.parseInt(stk.nextToken());
+				int b = Integer.parseInt(stk.nextToken());
+				
+				if (isUnion == 1) {
+					if (findSet(a) == findSet(b)) 
+						answer.append("1");
+					else 
+						answer.append("0");
+					
+				} else if (isUnion == 0){
+					union(a, b);
+					
+				}
+			}//for
+			
+			System.out.println(answer.toString());
+		}//tc
+	}
+	
+	private static int findSet(int a) {
+		if (parents[a] == -1)
+			return a;
+		return parents[a] = findSet(parents[a]);
+	}
+	
+	private static void union(int a, int b) {
+		int aRoot = findSet(a);
+		int bRoot = findSet(b);
+		
+		if (aRoot != bRoot) 
+			parents[bRoot] = aRoot;
+	}
+}
