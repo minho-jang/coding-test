93e166433029eff9f602531d5533d60bc471fc2b
minho-jang <minho9301@naver.com>
2021-03-29 22:01:39 +0900

    Solve: BOJ 1309 동물원, BOJ 11399 ATM, BOJ 16953 A->B

---
+++ src/baekjoon/Main_BOJ_16953_AtoB.java
@@ -0,0 +1,101 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+// A -> B
+public class Main_BOJ_16953_AtoB {
+	static int answer;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk;
+		
+		stk = new StringTokenizer(br.readLine());
+		long src = Integer.parseInt(stk.nextToken());
+		long dest = Integer.parseInt(stk.nextToken());
+		
+		answer = -1;
+		
+		// BFS
+		int level = 0;
+		Deque<Long> queue = new ArrayDeque<>();
+		queue.offer(src);
+		while(!queue.isEmpty()) {
+//			System.out.println("level: " + level);
+			
+			boolean hit = false;
+			int size = queue.size();
+			for (int i = 0; i < size; i++) {
+				long polled = queue.poll();
+//				System.out.print(polled + " ");
+				if (polled == dest) {
+					hit = true;
+					break;
+				}
+				if (polled > dest) {
+					continue;
+				}
+				
+				queue.offer(polled * 10 + 1);
+				queue.offer(polled * 2);
+			}
+//			System.out.println();
+			
+			if (hit) {
+				answer = level;
+				break;
+			}
+			
+			level++;
+		}
+		
+		if (answer >= 0)
+			answer++;
+		System.out.println(answer);
+	}
+}
+
+/*
+DFS 풀이
+
+// A -> B
+public class Main_BOJ_16953_AtoB2 {
+	static int answer;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk;
+		
+		stk = new StringTokenizer(br.readLine());
+		long src = Integer.parseInt(stk.nextToken());
+		long dest = Integer.parseInt(stk.nextToken());
+		
+		answer = -1;
+		dfs(src, dest, 0);
+		
+		if (answer >= 0)
+			answer++;
+		System.out.println(answer);
+	}
+	
+	private static void dfs(long cur, long dest, int count) {
+		if (cur > dest)
+			return;
+		if (cur == dest) {
+			if (answer >= 0)
+				answer = Math.min(answer, count);
+			else
+				answer = count;
+			return;
+		}
+		
+		dfs(cur*2, dest, count+1);
+		dfs(cur*10+1, dest, count+1);
+	}
+}
+*/
