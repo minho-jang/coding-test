ae7dc34dc31ca78a18e7cc53902e8e857d33bbe8
minho-jang <minho9301@naver.com>
2021-04-01 00:14:06 +0900

    Solve: BOJ 2026 소풍, 2609 최대공약수와최소공배수, 12904 A와B

---
+++ src/baekjoon/Main_BOJ_12904_A와B.java
@@ -0,0 +1,32 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_BOJ_12904_A와B {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String src = br.readLine();
+		String dest = br.readLine();
+		StringBuilder destSb = new StringBuilder(dest);
+		int destLen = dest.length();
+		int srcLen = src.length();
+		for (int i = destLen-1; i >= srcLen; i--) {
+			if (destSb.charAt(i) == 'A') {
+				destSb.deleteCharAt(i);
+				
+			} else if (destSb.charAt(i) == 'B') {
+				destSb.deleteCharAt(i);
+				destSb.reverse();
+				
+			}
+//			System.out.println(destSb.toString());
+		}
+		
+		if (destSb.toString().equals(src)) {
+			System.out.println(1);
+		} else
+			System.out.println(0);
+	}
+}
---
+++ src/baekjoon/Main_BOJ_2609_최대공약수와최소공배수.java
@@ -0,0 +1,89 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main_BOJ_2609_최대공약수와최소공배수 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		// 최대공약수
+		int gcm = 0;
+		int lg = Math.max(N, M);
+		int sm = Math.min(N, M);
+		while(true) {
+			int R = lg % sm;
+			if (R == 0) {
+				gcm = sm;
+				break;
+			} else {
+				lg = sm;
+				sm = R;
+			}
+		}
+		System.out.println(gcm);
+		
+		// 최소공배수
+		int lcm = N*M/gcm;
+		System.out.println(lcm);
+		
+		sc.close();
+	}
+}
+
+/*
+
+public class Main_BOJ_2609_최대공약수와최대공배수 {
+	private static ArrayList<Integer> primes;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		makePrime();
+		
+		// 최대 공약수
+		int tmpN = N;
+		int tmpM = M;
+		int gcm = 1;
+		while (true) {
+			boolean flag = false;
+			for (int p : primes) {
+				if (tmpN % p == 0 && tmpM % p == 0) {
+					gcm *= p;
+					tmpN /= p;
+					tmpM /= p;
+					flag = true;
+					break;
+				}
+			}
+			if (!flag)
+				break;
+		}
+		System.out.println(gcm);
+		
+		// 최소공배수
+		int lcm = gcm * tmpM * tmpN;
+		System.out.println(lcm);
+		
+		sc.close();
+	}
+
+	private static void makePrime() {
+		primes = new ArrayList<>();
+		int MAX = 10000;
+		boolean[] check = new boolean[MAX+1];
+		for (int i = 2; i <= MAX; i++) {
+			if (! check[i]) {
+				primes.add(i);
+				for (int j = i+i; j <= MAX; j+=i) {
+					check[j] = true;
+				}
+			}
+		}
+	}
+}
+
+*/
