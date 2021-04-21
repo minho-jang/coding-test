614e1f7a3b563ac92ce5f4f10d479b97428b5ec7
minho-jang <minho9301@naver.com>
2021-04-21 17:58:45 +0900

    Solve: BOJ 1194 달이차오른다가자

--- src/baekjoon/Main_BOJ_1194_달이차오른다가자.java
+++ src/baekjoon/Main_BOJ_1194_달이차오른다가자.java
@@ -94,3 +94,149 @@ public class Main_BOJ_1194_달이차오른다가자 {
 		System.out.println(answer);
 	}
 }
+
+/*
+
+public class Main_BOJ_1194_달이차오른다가자 {
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	static class Pos {
+		int r,c, moveCount;
+		int keyState;
+		public Pos(int r, int c, int moveCount, int keyState) {
+			this.r = r;
+			this.c = c;
+			this.moveCount = moveCount;
+			this.keyState = keyState;
+		}
+		@Override
+		public String toString() {
+			return "[" + r + ", " + c + ", " + keyState + "]";
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk;
+		
+		stk = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(stk.nextToken());
+		int M = Integer.parseInt(stk.nextToken());
+		
+		// 입력 받으면서 시작점을 찾고, 시작점은 '.'으로 바꿔준다.
+		Pos start = null;
+		char[][] map = new char[N][M];
+		for (int i = 0; i < N; i++) {
+			map[i] = br.readLine().toCharArray();
+			for (int j = 0; j < M; j++) {
+				if (map[i][j] == '0') {
+					start = new Pos(i, j, 0, 0);
+					map[i][j] = '.';
+				}
+			}
+		}
+		
+		// BFS
+		boolean[][][] visited = new boolean[N][M][64];
+		Queue<Pos> queue = new LinkedList<>();
+		
+		queue.add(start);
+		visited[start.r][start.c][0] = true;
+		
+		int answer = -1;
+		while(!queue.isEmpty()) {
+			Pos polled = queue.poll();
+			
+			for (int d = 0; d < 4; d++) {
+				int nr = polled.r + dr[d];
+				int nc = polled.c + dc[d];
+				
+				if (nr<0 || nr>=N || nc<0 || nc>=M)
+					continue;
+				if (visited[nr][nc][polled.keyState])
+					continue;
+				
+				if (map[nr][nc] == '1') {
+					answer = polled.moveCount + 1;
+					queue.clear();
+					break;
+				}
+				
+				else if (map[nr][nc] == '#') {
+					continue;
+				} else if (map[nr][nc] == '.') {
+					visited[nr][nc][polled.keyState] = true;
+					queue.offer(new Pos(nr, nc, polled.moveCount+1, polled.keyState));
+				}
+				
+				// 문 (A~F)
+				else if (map[nr][nc] == 'A') {
+					if ((polled.keyState & (1<<5)) == 1<<5) {		// 키 확인
+						visited[nr][nc][polled.keyState] = true;	// 방문처리
+						queue.offer(new Pos(nr, nc, polled.moveCount+1, polled.keyState)); // 큐에 삽입하여 계속 탐색
+					}
+				} else if (map[nr][nc] == 'B') {
+					if ((polled.keyState & (1<<4)) == 1<<4) {
+						visited[nr][nc][polled.keyState] = true;
+						queue.offer(new Pos(nr, nc, polled.moveCount+1, polled.keyState));
+					}
+				} else if (map[nr][nc] == 'C') {
+					if ((polled.keyState & (1<<3)) == 1<<3) {
+						visited[nr][nc][polled.keyState] = true;
+						queue.offer(new Pos(nr, nc, polled.moveCount+1, polled.keyState));
+					}
+				} else if (map[nr][nc] == 'D') {
+					if ((polled.keyState & (1<<2)) == 1<<2) {
+						visited[nr][nc][polled.keyState] = true;
+						queue.offer(new Pos(nr, nc, polled.moveCount+1, polled.keyState));
+					}
+				} else if (map[nr][nc] == 'E') {
+					if ((polled.keyState & (1<<1)) == 1<<1) {
+						visited[nr][nc][polled.keyState] = true;
+						queue.offer(new Pos(nr, nc, polled.moveCount+1, polled.keyState));
+					}
+				} else if (map[nr][nc] == 'F') {
+					if ((polled.keyState & (1<<0)) == 1<<0) {
+						visited[nr][nc][polled.keyState] = true;
+						queue.offer(new Pos(nr, nc, polled.moveCount+1, polled.keyState));
+					}
+				}
+				// 열쇠 (a~f)
+				else if (map[nr][nc] == 'a') {
+					int nKeyState = polled.keyState | (1<<5);		// 키 상태 변경
+					visited[nr][nc][nKeyState] = true;				// 방문처리
+					queue.offer(new Pos(nr, nc, polled.moveCount+1, nKeyState));	// 큐에 삽입하여 계속 탐색
+					
+				} else if (map[nr][nc] == 'b') {
+					int nKeyState = polled.keyState | (1<<4);
+					visited[nr][nc][nKeyState] = true;
+					queue.offer(new Pos(nr, nc, polled.moveCount+1, nKeyState));
+					
+				} else if (map[nr][nc] == 'c') {
+					int nKeyState = polled.keyState | (1<<3);
+					visited[nr][nc][nKeyState] = true;
+					queue.offer(new Pos(nr, nc, polled.moveCount+1, nKeyState));
+					
+				} else if (map[nr][nc] == 'd') {
+					int nKeyState = polled.keyState | (1<<2);
+					visited[nr][nc][nKeyState] = true;
+					queue.offer(new Pos(nr, nc, polled.moveCount+1, nKeyState));
+					
+				} else if (map[nr][nc] == 'e') {
+					int nKeyState = polled.keyState | (1<<1);
+					visited[nr][nc][nKeyState] = true;
+					queue.offer(new Pos(nr, nc, polled.moveCount+1, nKeyState));
+					
+				} else if (map[nr][nc] == 'f') {
+					int nKeyState = polled.keyState | (1<<0);
+					visited[nr][nc][nKeyState] = true;
+					queue.offer(new Pos(nr, nc, polled.moveCount+1, nKeyState));
+				}
+			}
+		}
+		
+		System.out.println(answer);
+	}
+}
+
+
+*/
