5dc7e6a825e8722792e6bb7bb4467aa66c6dcaab
장민호 <minho9301@naver.com>
2021-04-22 21:31:54 +0900

    Solve: PRG 조이스틱, 타겟넘버

---
+++ src/programmers/Solution_PRG_타겟넘버.java
@@ -0,0 +1,28 @@
+package programmers;
+
+public class Solution_PRG_타겟넘버 {
+	static int[] numbers;
+    static int answer, N, target;
+
+    public int solution(int[] numbers, int target) {
+        this.numbers = numbers;
+        this.N = numbers.length;
+        this.target = target;
+
+        dfs(0, 0);
+
+        return answer;
+    }
+
+    private static void dfs(int cnt, int result) {
+        if (cnt == N) {
+            if (result == target)
+                answer++;
+            return;
+        }
+
+        dfs(cnt+1, result+numbers[cnt]);
+        dfs(cnt+1, result-numbers[cnt]);
+    }
+}
+
