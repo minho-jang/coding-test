9e2f2849c2fedb1ec957821b5633feaaa7664921
장민호 <minho9301@naver.com>
2021-04-22 22:58:24 +0900

    Solve: PRG 124나라의숫자, 숫자의표현

---
+++ src/programmers/Solution_PRG_숫자의표현.java
@@ -0,0 +1,31 @@
+package programmers;
+
+public class Solution_PRG_숫자의표현 {
+	public int solution(int n) {
+        int answer = 0;
+        
+        int start = 1;
+        int end = 1;
+        int sum = 1;
+        while(true) {
+            if (start == n) {
+                answer++;
+                break;
+            }
+            
+            if (sum < n) {
+                sum += ++end;
+                
+            } else if (sum > n) {
+                sum -= start++;
+                
+            } else {
+                answer++;
+                sum += ++end;
+                
+            }
+        }
+        
+        return answer;
+    }
+}
