edbcc02fbd69baa059aee95959c373e87db8cbf1
minho-jang <minho9301@naver.com>
2021-05-10 18:20:38 +0900

    Solve: BOJ 1717 집합의표현, PRG 2020_Kakao 괄호변환, 문자열압축, 자물쇠와열쇠

---
+++ src/baekjoon/Main_BOJ_1717_집합의표현.java
@@ -0,0 +1,54 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BOJ_1717_집합의표현 {
+	private static int[] parents;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk;
+		
+		stk = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(stk.nextToken());
+		int m = Integer.parseInt(stk.nextToken());
+		
+		parents = new int[n+1];
+		for (int i = 0; i <= n; i++) {
+			parents[i] = i;
+		}
+		
+		for (int i = 0; i < m; i++) {
+			stk = new StringTokenizer(br.readLine());
+			int check = Integer.parseInt(stk.nextToken());
+			int a = Integer.parseInt(stk.nextToken());
+			int b = Integer.parseInt(stk.nextToken());
+			
+			if (check==0) {
+				union(a, b);
+			} else if (check==1) {
+				if (find(a)==find(b)) {
+					System.out.println("YES");
+				} else {
+					System.out.println("NO");
+				}
+			}
+		}
+	}
+
+	private static int find(int a) {
+		if (parents[a] == a) 
+			return a;
+		
+		return parents[a] = find(parents[a]);
+	}
+
+	private static void union(int a, int b) {
+		int pa = find(a);
+		int pb = find(b);
+		parents[pb] = pa;
+	}
+}
---
+++ src/programmers/Solution_PRG_2020Kakao_자물쇠와열쇠.java
@@ -0,0 +1,85 @@
+package programmers;
+
+import java.util.*;
+
+class Solution_PRG_2020Kakao_자물쇠와열쇠 {
+        public boolean solution(int[][] key, int[][] lock) {
+        boolean answer = false;
+        
+        for (int i=0; i<4; i++) {
+            if (i > 0)
+                key = rotate90(key);
+            if (isFit(key, lock)) {
+                answer = true;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+    
+    int[][] rotate90(int[][] arr) {
+        int N = arr.length;
+        int[][] rotated = new int[N][N];
+        
+        for (int c=0; c<N; c++) {
+            for (int r=0; r<N; r++) {
+                rotated[r][c] = arr[N-c-1][r];
+            }
+        }
+        
+        return rotated;
+    }
+    
+    boolean isFit(int[][] key, int[][] lock) {
+        int keyN = key.length;
+        int lockN = lock.length;
+        
+        int N = 2*keyN + lockN - 2;
+        int[][] bigLock = new int[N][N];
+        for (int i=0; i<N; i++) {
+            Arrays.fill(bigLock[i], 1);
+        }
+        for (int i=0; i<lockN; i++) {
+            for (int j=0; j<lockN; j++) {
+                bigLock[i+keyN-1][j+keyN-1] = lock[i][j];
+            }
+        }
+        
+        for (int i=0; i<keyN+lockN-1; i++) {
+            for (int j=0; j<keyN+lockN-1; j++) {
+                
+                for (int ki=0; ki<keyN; ki++) {
+                    for (int kj=0; kj<keyN; kj++) {
+                        if (key[ki][kj] == 1)
+                            bigLock[i+ki][j+kj] += 1;
+                    }
+                }
+                
+                if (isAllRight(bigLock, lockN, keyN))
+                    return true;
+                
+                for (int ki=0; ki<keyN; ki++) {
+                    for (int kj=0; kj<keyN; kj++) {
+                        if (key[ki][kj] == 1)
+                            bigLock[i+ki][j+kj] -= 1;
+                    }
+                }
+                
+            }
+        }
+        
+        return false;
+    }
+    
+    boolean isAllRight(int[][] lock, int lockN, int keyN) {
+        for (int i=0; i<lockN; i++) {
+            for (int j=0; j<lockN; j++) {
+                if (lock[i+keyN-1][j+keyN-1] != 1)
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+}
