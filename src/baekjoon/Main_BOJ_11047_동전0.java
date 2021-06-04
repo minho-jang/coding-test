fd21e43f489ab2f72ef1574b137a223ae8cb470e
장민호 <minho9301@naver.com>
2021-06-04 22:49:49 +0900

    Solve: BOJ 11047 동전0

---
+++ src/baekjoon/Main_BOJ_11047_동전0.java
@@ -0,0 +1,37 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BOJ_11047_동전0 {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer stk;
+    
+    stk = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(stk.nextToken());
+    int K = Integer.parseInt(stk.nextToken());
+    
+    int[] arr = new int[N];
+    for (int i=0; i<N; i++) {
+      arr[i] = Integer.parseInt(br.readLine());
+    }
+    
+    int answer = 0;
+    int idx = N-1;
+    while(K != 0 && idx >= 0) {
+      if (K / arr[idx] > 0) {
+        K -= arr[idx];
+        answer++;
+        
+      } else {
+        idx--;
+      }
+    }
+    
+    System.out.println(answer);
+    
+    br.close();
+  }
+}
