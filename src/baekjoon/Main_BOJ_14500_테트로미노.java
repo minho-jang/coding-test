df58c3227c8c687d92bbc181793379d1bb63320f
minho-jang <minho9301@naver.com>
2021-06-08 13:28:55 +0900

    Solve: BOJ 14500 테트로미노

--- src/baekjoon/Main_BOJ_14500_테트로미노.java
+++ src/baekjoon/Main_BOJ_14500_테트로미노.java
@@ -4,9 +4,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main_BOJ_14500_테트로미노 {
-  static int answer, N, M;
-  static int[][] arr;
+  static int N, M, answer;
+  static int[][] map;
   static boolean[][] visited;
+
+  // ㅗ에서 꼬다리를 기준으로 4칸의 상대적 좌표
+  static int[][][] OH = { 
+      { { 0, 0 }, { 1, -1 }, { 1, 0 }, { 1, 1 } },   // ㅗ
+      { { 0, 0 }, { -1, -1}, { 0, -1}, { 1, -1} },   // ㅏ
+      { { 0, 0 }, { -1, 1 }, { 0, 1 }, { 1, 1 } },   // ㅓ
+      { { 0, 0 }, { -1, -1}, { -1, 0}, { -1, 1} },   // ㅜ
+  };
+  // 4방탐색 좌표
   static int[] dr = { -1, 1, 0, 0 };
   static int[] dc = { 0, 0, -1, 1 };
 
@@ -17,89 +26,69 @@ public class Main_BOJ_14500_테트로미노 {
     stk = new StringTokenizer(br.readLine());
     N = Integer.parseInt(stk.nextToken());
     M = Integer.parseInt(stk.nextToken());
-    arr = new int[N][M];
-    visited = new boolean[N][M];
 
+    map = new int[N][M];
+    visited = new boolean[N][M];
     for (int i = 0; i < N; i++) {
       stk = new StringTokenizer(br.readLine());
       for (int j = 0; j < M; j++) {
-        arr[i][j] = Integer.parseInt(stk.nextToken());
+        map[i][j] = Integer.parseInt(stk.nextToken());
       }
     }
 
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < M; j++) {
-        dfs(0, i, j, 0); // 'ㅗ' 모양을 제외한 테트로미노 확인
-        checkOH(i, j); // 'ㅗ' 모양 확인
+        // 각 좌표별로, 4칸만 DFS (ㅗ를 제외한 나머지 확인 가능)
+        visited[i][j] = true;
+        dfs(i, j, 0, 0);
+        visited[i][j] = false;
+
+        // ㅗ 확인
+        checkOh(i, j);
       }
     }
 
     System.out.println(answer);
   }
 
-  // r, c 위치를 기준으로 'ㅗ'모양 확인. 튀어나온 부분이 (r, c)라고 가정
-  private static void checkOH(int r, int c) {
-    int result = 0;
+  private static void checkOh(int r, int c) {
+    for (int i = 0; i < 4; i++) {
+      int[][] oh = OH[i];             // ㅗ, ㅓ, ㅏ, ㅜ 중 하나
+      int result = 0;
+      
+      for (int j = 0; j < 4; j++) {   // (r,c)를 기준으로 ㅗ를 만드는데
+        int nr = r + oh[j][0];
+        int nc = c + oh[j][1];
+        if (isOut(nr, nc))  break;    // 범위를 벗어나면 빠져나가
 
-    try {
-      // ㅗ
-      result = arr[r][c];
-      result += arr[r + 1][c - 1];
-      result += arr[r + 1][c];
-      result += arr[r + 1][c + 1];
-      answer = Math.max(answer, result);
-    } catch (Exception e) {
-//    System.out.println(e.getMessage());
-    }
-    try {
-      // ㅓ
-      result = arr[r][c];
-      result += arr[r - 1][c + 1];
-      result += arr[r][c + 1];
-      result += arr[r + 1][c + 1];
-      answer = Math.max(answer, result);
-    } catch (Exception e) {
-//    System.out.println(e.getMessage());
-    }
-    try {
-      // ㅜ
-      result = arr[r][c];
-      result += arr[r - 1][c - 1];
-      result += arr[r - 1][c];
-      result += arr[r - 1][c + 1];
-      answer = Math.max(answer, result);
-    } catch (Exception e) {
-//    System.out.println(e.getMessage());
-    }
-    try {
-      // ㅏ
-      result = arr[r][c];
-      result += arr[r - 1][c - 1];
-      result += arr[r][c - 1];
-      result += arr[r + 1][c - 1];
-      answer = Math.max(answer, result);
-
-    } catch (Exception e) {
-//      System.out.println(e.getMessage());
+        result += map[nr][nc];        // 해당 모양의 점수 확인
+      }
+      
+      answer = Math.max(result, answer);
     }
   }
 
-  private static void dfs(int depth, int r, int c, int score) {
+  private static boolean isOut(int r, int c) {
+    return r < 0 || r >= N || c < 0 || c >= M;
+  }
+
+  private static void dfs(int r, int c, int depth, int score) {
     if (depth == 4) {
       answer = Math.max(answer, score);
       return;
     }
 
+    // DFS 하면서 점수 누적 
+    score += map[r][c];
+
     for (int d = 0; d < 4; d++) {
       int nr = r + dr[d];
       int nc = c + dc[d];
-      if (nr < 0 || nr >= N || nc < 0 || nc >= M)
-        continue;
-      if (visited[nr][nc])
-        continue;
+      if (isOut(nr, nc))    continue;
+      if (visited[nr][nc])  continue;
 
       visited[nr][nc] = true;
-      dfs(depth + 1, nr, nc, score + arr[nr][nc]);
+      dfs(nr, nc, depth + 1, score);
       visited[nr][nc] = false;
     }
   }
