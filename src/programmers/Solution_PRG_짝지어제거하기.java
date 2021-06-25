23da7160f253fcf047272078aa97fcb7e60da777
장민호 <minho9301@naver.com>
2021-06-25 20:10:54 +0900

    Solve: PRG 짝지어제거하기

---
+++ src/programmers/Solution_PRG_짝지어제거하기.java
@@ -0,0 +1,40 @@
+package programmers;
+
+import java.util.*;
+
+public class Solution_PRG_짝지어제거하기 {
+  
+  public int solution(String s) {
+      int answer = -1;
+      
+      Stack<Character> stack = new Stack<>();
+      for (int i=0; i<s.length(); i++) {
+        char cur = s.charAt(i);
+        if (stack.isEmpty()) {
+          stack.push(cur);
+          continue;
+        }
+        
+        char peeked = stack.peek();
+        if (peeked == cur)
+          stack.pop();       
+        else 
+          stack.push(cur);
+      }
+      
+      if (stack.isEmpty())
+        answer = 1;
+      else
+        answer = 0;
+
+      return answer;
+  }
+  
+  public static void main(String[] args) {
+    Solution_PRG_짝지어제거하기 sol = new Solution_PRG_짝지어제거하기();
+    
+    String s = "cdcd";
+    System.out.println(sol.solution(s));
+  }
+
+}
