51ec25e1abd66003dfed2d9a5ee06c0bc1e7ae94
minho-jang <minho9301@naver.com>
2021-07-14 18:55:20 +0900

    Solve: PRG 2021_Kakao_Intern 표편집

--- src/programmers/Solution_PRG_2021KakaoIntern_표편집.java
+++ src/programmers/Solution_PRG_2021KakaoIntern_표편집.java
@@ -3,26 +3,22 @@ package programmers;
 import java.util.*;
 
 public class Solution_PRG_2021KakaoIntern_표편집 {
-  private LinkedList<Integer> table;
-  private int cursor;
+  private int cursor, tableSize;
   private Stack<Integer> stack;
 
   public String solution(int n, int k, String[] cmd) {
-    table = new LinkedList<>();
     cursor = k;
+    tableSize = n;
     stack = new Stack<>();
-    for (int i = 0; i < n; i++)
-      table.add(i);
 
     for (String command : cmd)
       doCmd(command);
 
     StringBuilder answerSb = new StringBuilder();
-    for (int i=0; i<n; i++)
-      answerSb.append("X");
-
-    for (int data : table)
-      answerSb.replace(data, data+1, "O");
+    for (int i=0; i<tableSize; i++)
+      answerSb.append("O");
+    while(!stack.isEmpty())
+      answerSb.insert(stack.pop(), "X");
 
     return answerSb.toString();
   }
@@ -30,11 +26,11 @@ public class Solution_PRG_2021KakaoIntern_표편집 {
   private void doCmd(String command) {
     char behavior = command.charAt(0);
     if (behavior == 'U') {
-      int move = command.charAt(2) - '0';
+      int move = Integer.parseInt(command.substring(2));
       moveUp(move);
 
     } else if (behavior == 'D') {
-      int move = command.charAt(2) - '0';
+      int move = Integer.parseInt(command.substring(2));
       moveDown(move);
 
     } else if (behavior == 'C') {
@@ -43,51 +39,36 @@ public class Solution_PRG_2021KakaoIntern_표편집 {
     } else if (behavior == 'Z') {
       restoreData();
     }
-
-//    System.out.print(table);
-//    System.out.println("\t" + cursor + "\t\t" + command);
   }
 
   private void restoreData() {
     int pos = stack.pop();
-    int data = stack.pop();
-    table.add(pos, data);
+    tableSize++;
     if (pos <= cursor)
       moveDown(1);
   }
 
   private void deleteData() {
-    int removed = table.remove(cursor);
-    stack.push(removed);
     stack.push(cursor);
-    if (cursor == table.size())
+    tableSize--;
+    if (cursor == tableSize)
       moveUp(1);
   }
 
   private void moveUp(int move) {
-    if (cursor - move < 0)
-      cursor = 0;
-    else
-      cursor -= move;
+    cursor -= move;
   }
 
   private void moveDown(int move) {
-    if (cursor + move >= table.size())
-      cursor = table.size() - 1;
-    else
-      cursor += move;
+    cursor += move;
   }
 
   public static void main(String[] args) {
     Solution_PRG_2021KakaoIntern_표편집 sol = new Solution_PRG_2021KakaoIntern_표편집();
-//    int n = 8;
-//    int k = 2;
-//    String[] cmd = {"D 2","C","U 3","C","D 4","C","U 2","Z","Z"};
+    int n = 8;
+    int k = 2;
+    String[] cmd = {"D 2","C","U 3","C","D 4","C","U 2","Z","Z"};
 //    String[] cmd = {"D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"};
-
-    int n = 5;
-    int k = 0;
-    String[] cmd = {"C", "C", "Z", "Z", "C", "C", "Z", "Z", "C", "C", "C", "C", "Z", "Z", "C", "Z", "Z"};
 
     System.out.println(sol.solution(n, k, cmd));
   }
