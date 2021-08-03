1c82643ad68c35414084a9d25db4c1cbe5f090a8
minho-jang <minho9301@naver.com>
2021-08-03 16:19:05 +0900

    Solve: BOJ 2606 바이러스

---
+++ src/baekjoon/Main_BOJ_2606_바이러스.java
@@ -0,0 +1,44 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BOJ_2606_바이러스 {
+	private static int answer = 0;
+	private static int N;
+	private static int[][] adjMatrix;
+	private static boolean[] visited;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk;
+
+		N = Integer.parseInt(br.readLine());
+		int edgeNum = Integer.parseInt(br.readLine());
+
+		adjMatrix = new int[N][N];
+		for (int i=0; i<edgeNum; i++) {
+			stk = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(stk.nextToken()) - 1;
+			int to = Integer.parseInt(stk.nextToken()) - 1;
+			adjMatrix[from][to] = adjMatrix[to][from] = 1;
+		}
+
+		visited = new boolean[N];
+		dfs(0);
+
+		System.out.println(answer);
+	}
+
+	private static void dfs(int start) {
+		visited[start] = true;
+		for (int i=0; i<N; i++) {
+			if (adjMatrix[start][i] == 1 && !visited[i]) {
+				answer++;
+				dfs(i);
+			}
+		}
+	}
+}
